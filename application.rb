33fb8755411c0ef7b1deb08f4877bfe9f8094972
Karel Minarik <karmi@karmi.cz>
2010-04-22 15:47:22 +0200

    Pridana funkce vytvareni stranek: <form> pro novou, handler /POST

--- application.rb
+++ application.rb
@@ -1,6 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'fileutils'
+
+PAGES_DIRECTORY = File.expand_path('../pages', __FILE__)
+
+FileUtils.mkdir_p PAGES_DIRECTORY
 
 get '/' do
   "Welcome to our wiki"
+end
+
+get '/new' do
+  erb :form
+end
+
+post '/pages' do
+
+  filename = params['title']
+
+  puts "*** Creating page #{filename}"
+
+  File.open PAGES_DIRECTORY + '/' + filename, 'w' do |file|
+    file.write params['body']
+  end
+
+  redirect '/'
 end
